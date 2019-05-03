c0430667aeeb15b68958b20e11a2e0522d777810
Antonio Filgueras <antonio.filgueras@bsc.es>
2019-05-03 12:31:25 +0200

    Create platform directories
    Create platform directories so we can have xdma implementations for different platforms

---
+++ src/zynq/libxdma_version_template.h
@@ -0,0 +1,9 @@
+#ifndef LIBXDMA_VERSION_H
+#define LIBXDMA_VERSION_H
+
+#define LIBXDMA_VERSION_MAJOR 3
+#define LIBXDMA_VERSION_MINOR 0
+
+//This comment should be replaced by the commit build info after the "make install" step
+
+#endif
