5b886735535f1bbc34d5e2d8773a4809dfe3e678
Jaume Bosch <jaume.bosch@bsc.es>
2019-10-08 14:44:08 +0200

    Bump version to 3.1
    This is due to the API changes in commit decc8ef2e26b32891180ac7b1d1487f785133921

--- src/libxdma_version_template.h
+++ src/libxdma_version_template.h
@@ -2,7 +2,7 @@
 #define LIBXDMA_VERSION_H
 
 #define LIBXDMA_VERSION_MAJOR 3
-#define LIBXDMA_VERSION_MINOR 0
+#define LIBXDMA_VERSION_MINOR 1
 
 //This comment should be replaced by the commit build info after the "make install" step
 
