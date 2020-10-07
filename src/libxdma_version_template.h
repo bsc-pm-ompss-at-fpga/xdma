759e28a3d6158db0687c044793a2728c6ac71d5d
Antonio Filgueras <antonio.filgueras@bsc.es>
2020-10-07 12:57:18 +0200

    Bump version to 3.7

--- src/libxdma_version_template.h
+++ src/libxdma_version_template.h
@@ -2,7 +2,7 @@
 #define LIBXDMA_VERSION_H
 
 #define LIBXDMA_VERSION_MAJOR 3
-#define LIBXDMA_VERSION_MINOR 6
+#define LIBXDMA_VERSION_MINOR 7
 
 //This comment should be replaced by the commit build info after the "make install" step
 
