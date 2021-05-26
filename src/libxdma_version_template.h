2e797ef60743045616c1b468c2bdb42934d5d124
Antonio Filgueras <antonio.filgueras@bsc.es>
2021-05-26 15:36:04 +0200

    Bump version to 3.13

--- src/libxdma_version_template.h
+++ src/libxdma_version_template.h
@@ -22,7 +22,7 @@
 #define LIBXDMA_VERSION_H
 
 #define LIBXDMA_VERSION_MAJOR 3
-#define LIBXDMA_VERSION_MINOR 12
+#define LIBXDMA_VERSION_MINOR 13
 
 //This comment should be replaced by the commit build info after the "make install" step
 
