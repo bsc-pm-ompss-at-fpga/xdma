063031694842fdc0c7c9b0774d9b98b190d4c84b
Miquel Vidal Piñol <miquel.vidal@bsc.es>
2022-07-26 08:47:51 +0200

    Bump version to 4.1

--- src/libxdma_version_template.h
+++ src/libxdma_version_template.h
@@ -22,7 +22,7 @@
 #define LIBXDMA_VERSION_H
 
 #define LIBXDMA_VERSION_MAJOR 4
-#define LIBXDMA_VERSION_MINOR 0
+#define LIBXDMA_VERSION_MINOR 1
 
 //This comment should be replaced by the commit build info after the "make install" step
 
