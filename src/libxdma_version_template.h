3908e0043d1f9b8c926775d507786aab70966303
Juan Miguel de Haro Ruiz <juan.deharoruiz@bsc.es>
2024-06-11 15:32:44 +0200

    Bump version to 4.7

--- src/libxdma_version_template.h
+++ src/libxdma_version_template.h
@@ -22,7 +22,7 @@
 #define LIBXDMA_VERSION_H
 
 #define LIBXDMA_VERSION_MAJOR 4
-#define LIBXDMA_VERSION_MINOR 6
+#define LIBXDMA_VERSION_MINOR 7
 
 //This comment should be replaced by the commit build info after the "make install" step
 
